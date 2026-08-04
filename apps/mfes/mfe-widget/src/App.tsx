f1dc6f330d659bdede357dc9fb8886d67b235596
Ali Alvi <ali.raza@tss.se>
2026-08-04 14:14:51 +0200

    chore(mfe-widget): update dev rendering log message

--- apps/mfes/mfe-widget/src/App.tsx
+++ apps/mfes/mfe-widget/src/App.tsx
@@ -32,7 +32,7 @@ export default function App({
 
   if (import.meta.env.DEV) {
     console.log(
-      `[MFE-Widget] Rendering widget mfe with basePath="${basePath}", router="${routerType}"`,
+      `[MFE-Widget] Rendering widget app mfe with basePath="${basePath}", router="${routerType}"`,
     );
     console.log(`[MFE-Widget] Auth state:`, { isAuthenticated, user: user?.email });
   }
